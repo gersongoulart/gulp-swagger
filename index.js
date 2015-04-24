9d9800453be0472af212d6f93b489d9ed122c366
Gerson Goulart <gerson.goulart@gmail.com>
2015-04-23 17:36:00 -0700

    Add more comments to code and allow for shortcuts when passing templates to codeGen.

--- index.js
+++ index.js
@@ -6,38 +6,71 @@ var CodeGen = require('swagger-js-codegen').CodeGen;
 var PLUGIN_NAME = 'gulp-swagger';
 
 module.exports = function gulpSwagger (filename, options) {
+  // Allow for passing the `filename` as part of the options.
   if ( 'object' === typeof filename ) {
     options = filename;
     filename = options.filename;
   }
 
+  // File name is mandatory (otherwise gulp won't be able to write the file properly)
   if (!filename) {
     throw new gutil.PluginError(PLUGIN_NAME, 'A file name is required');
   }
 
   options = options || {};
 
+  // Flag if user actually wants to use codeGen or just parse the schema and get json back.
   var useCodeGen = 'object' === typeof options.codegen;
   var codeGenSettings;
 
+  // If user wants to use the codeGen
   if (useCodeGen) {
+    // Allow for shortcuts by providing sensitive defaults.
     codeGenSettings = options.codegen || {};
-    codeGenSettings.type = codeGenSettings.type || 'custom'; // type of codeGen, either: 'angular', 'node' or 'custom'
+    codeGenSettings.type = codeGenSettings.type || 'custom'; // type of codeGen, either: 'angular', 'node' or 'custom'.
     codeGenSettings.moduleName = codeGenSettings.moduleName || 'API';
     codeGenSettings.className = codeGenSettings.className || 'API';
 
-    if ( 'object' === typeof codeGenSettings.template ) {
-      var classTmpl = fs.readFileSync(codeGenSettings.template.class, 'utf-8');
-      var methodTmpl = fs.readFileSync(codeGenSettings.template.method, 'utf-8');
-      var requestTmpl = fs.readFileSync(codeGenSettings.template.request, 'utf-8');
+    // If codeGen is of type custom, user must provide templates.
+    if ( codeGenSettings.type === 'custom' && !codeGenSettings.template ) {
+      throw new gutil.PluginError(PLUGIN_NAME, 'Templates are mandatory for a custom codegen');
+    }
 
-      if (!classTmpl || !methodTmpl || !requestTmpl) {
-        throw new gutil.PluginError(PLUGIN_NAME, 'Could not load all template files');
+    // Shortcut: Allow `template` to be a string passing a single template file.
+    else if ( 'string' === typeof codeGenSettings.template ) {
+      var template = fs.readFileSync(codeGenSettings.template, 'utf-8');
+
+      if ( 'string' !== typeof template || !template.length ) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Could not load template file');
       }
 
-      codeGenSettings.template.class = classTmpl;
-      codeGenSettings.template.method = methodTmpl;
-      codeGenSettings.template.request = requestTmpl;
+      codeGenSettings.template = {
+        class: template,
+        method: '',
+        request: ''
+      }
+    }
+
+    // Regular codeGen template object, but allowing for missing templates.
+    // (e.g. use case: if `request` is too simple, there's no need for a dedicated template file)
+    else if ( 'object' === typeof codeGenSettings.template ) {
+      function loadTemplateFile (tmpl) {
+        var template = codeGenSettings.template[tmpl];
+
+        if ( 'string' !== typeof template || !template.length ) {
+          return codeGenSettings.template[tmpl] = '';
+        }
+
+        template = fs.readFileSync(template, 'utf-8');
+
+        if ( 'string' !== typeof template || !template.length ) {
+          throw new gutil.PluginError(PLUGIN_NAME, 'Could not load ' + tmpl + ' template file. Please make sure path to file is correct.');
+        }
+
+        codeGenSettings.template[tmpl] = template;
+      }
+
+      ['class', 'method', 'request'].forEach(loadTemplateFile);
     }
   }
 
